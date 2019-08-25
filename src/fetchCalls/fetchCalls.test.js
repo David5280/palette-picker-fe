473d0855390148e900cb707e1e7f19890ce657e8
Patrick Goulding <patrickgoulding@gmail.com>
2019-08-25 16:18:57 -0600

    start with tests for fetch call

---
+++ src/fetchCalls/fetchCalls.test.js
@@ -0,0 +1,158 @@
+import { serverCall } from './fetchCalls';
+import * as mockData from '../mockData';
+
+describe('fetchCalls', () => {
+
+  it('SAD: Should return an error if no params are sent', () => {
+
+  })
+
+  describe('GET projects', () => {
+
+    beforeEach(() => {
+      window.fetch = jest.fn().mockImplementation(() =>
+        Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(
+            [...mockData.mockProjects]
+          )
+        })
+      );
+    });
+
+    it('HAPPY: Should call the fetch with the correct arguements', async () => {
+      const mockUrl = `https://pallete-picker-de-pg.herokuapp.com/api/v1/projects`
+      const mockPath = 'projects'
+      await serverCall(mockPath);
+      expect(window.fetch).toHaveBeenCalledWith(mockUrl);
+    });
+
+    it('HAPPY: Should return all the projects', async () => {
+
+    })
+
+  })
+
+  describe('GET palettes', () => {
+
+    beforeEach(() => {
+      window.fetch = jest.fn().mockImplementation(() =>
+        Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(
+            mockData.mockPalettes
+          )
+        })
+      );
+    });
+
+    it('HAPPY: Should call the fetch with the correct arguments', async () => {
+      const mockUrl = `https://pallete-picker-de-pg.herokuapp.com/api/v1/palettes`
+      const mockPath = 'palettes'
+      await serverCall(mockPath);
+      expect(window.fetch).toHaveBeenCalledWith(mockUrl);
+    });
+
+    it('HAPPY: should return all the palettes', async () => {
+
+    })
+
+  })
+
+  describe('GET single Palette', async () => {
+    
+    it('HAPPY: Should call the fetch with the correct arguments', async () => {
+      const mockUrl = `https://pallete-picker-de-pg.herokuapp.com/api/v1/palettes`
+      const mockPath = 'palettes/1'
+      await serverCall(mockPath);
+      expect(window.fetch).toHaveBeenCalledWith(mockUrl);
+    });
+
+    it('HAPPY:should find the single palette specified', () => {
+      const mockPath = 'https://pallete-picker-de-pg.herokuapp.com/api/v1/palettes'
+
+    })
+
+    it('SAD: Should return an error if no palette with matching ID', async () => {
+
+    })
+
+  })
+
+  describe('POST to palletes', () => {
+
+    it('HAPPY: Should call the fetch with the correct arguments', async () => {
+
+    });
+
+    it('HAPPY: Should post to the pallettes', async () => {
+
+    })
+
+    it('SAD: Should return an error if there is a missing value', async () => {
+
+    })
+
+  })
+
+  describe('POST to projects', () => {
+
+    it('HAPPY: Should post a project', () => {
+
+    })
+
+    it('SAD: Should return an error if no name has been sent', () => {
+
+    })
+
+  })
+
+  describe('DELETE a palette', () => {
+
+    it('HAPPY: Should delete a palette', () => {
+
+    })
+
+    it('SAD: Should return an error if no palette matches', () => {
+
+    })
+
+  })
+
+  describe('DELETE a project', () => {
+
+    it('HAPPY: Should delete a project', () => {
+
+    })
+
+    it('SAD: Should return an error if no project found', () => {
+
+    })
+
+  })
+
+  describe('DELETE a palette', () => {
+
+    it('HAPPY: Should delete a palette', () => {
+
+    })
+
+    it('SAD: Should return an error if no palette matches', () => {
+
+    })
+
+  })
+
+  describe('DELETE a project', () => {
+
+    it('HAPPY: Should delete a project', () => {
+
+    })
+
+    it('SAD: Should return an error if no project found', () => {
+
+    })
+
+  }) 
+
+});
---
+++ src/mockData.js
@@ -0,0 +1,153 @@
+export const mockProjects= [
+  {
+    "id": 1,
+    "name": "Artisinal Cheese Shop",
+    "created_at": "2019-08-25T21:04:48.793Z",
+    "updated_at": "2019-08-25T21:04:48.793Z"
+  },
+  {
+    "id": 3,
+    "name": "Butcher Shop",
+    "created_at": "2019-08-25T21:04:48.798Z",
+    "updated_at": "2019-08-25T21:04:48.798Z"
+  },
+  {
+    "id": 2,
+    "name": "Barber",
+    "created_at": "2019-08-25T21:04:48.798Z",
+    "updated_at": "2019-08-25T21:04:48.798Z"
+  }
+]
+
+export const singleMockProject = {
+  "id": 1,
+  "name": "Artisinal Cheese Shop",
+  "created_at": "2019-08-25T21:04:48.793Z",
+  "updated_at": "2019-08-25T21:04:48.793Z",
+  "palette": [
+    {
+      "id": 1,
+      "name": "Warm",
+      "created_at": "2019-08-25T21:04:48.835Z",
+      "updated_at": "2019-08-25T21:04:48.835Z",
+      "color_1": "#000123",
+      "color_2": "#900123",
+      "color_3": "#J00123",
+      "color_4": "#000123",
+      "color_5": "#000123",
+      "project_id": 1
+    },
+    {
+      "id": 2,
+      "name": "Cool",
+      "created_at": "2019-08-25T21:04:48.836Z",
+      "updated_at": "2019-08-25T21:04:48.836Z",
+      "color_1": "#H00123",
+      "color_2": "#A00123",
+      "color_3": "#G00123",
+      "color_4": "#670123",
+      "color_5": "#000123",
+      "project_id": 1
+    }
+  ]
+}
+
+export const mockPalettes = [
+  {
+    "id": 1,
+    "name": "Warm",
+    "color_1": "#000123",
+    "color_2": "#900123",
+    "color_3": "#J00123",
+    "color_4": "#000123",
+    "color_5": "#000123",
+    "project_id": 1,
+    "created_at": "2019-08-25T21:04:48.835Z",
+    "updated_at": "2019-08-25T21:04:48.835Z"
+  },
+  {
+    "id": 3,
+    "name": "Oblique",
+    "color_1": "#600123",
+    "color_2": "#000123",
+    "color_3": "#120123",
+    "color_4": "#006123",
+    "color_5": "#430123",
+    "project_id": 3,
+    "created_at": "2019-08-25T21:04:48.837Z",
+    "updated_at": "2019-08-25T21:04:48.837Z"
+  },
+  {
+    "id": 2,
+    "name": "Cool",
+    "color_1": "#H00123",
+    "color_2": "#A00123",
+    "color_3": "#G00123",
+    "color_4": "#670123",
+    "color_5": "#000123",
+    "project_id": 1,
+    "created_at": "2019-08-25T21:04:48.836Z",
+    "updated_at": "2019-08-25T21:04:48.836Z"
+  },
+  {
+    "id": 4,
+    "name": "Winter",
+    "color_1": "#000123",
+    "color_2": "#000123",
+    "color_3": "#120123",
+    "color_4": "#005123",
+    "color_5": "#430123",
+    "project_id": 3,
+    "created_at": "2019-08-25T21:04:48.840Z",
+    "updated_at": "2019-08-25T21:04:48.840Z"
+  },
+  {
+    "id": 5,
+    "name": "Autumn",
+    "color_1": "#010123",
+    "color_2": "#043123",
+    "color_3": "#430123",
+    "color_4": "#000123",
+    "color_5": "#300123",
+    "project_id": 2,
+    "created_at": "2019-08-25T21:04:48.841Z",
+    "updated_at": "2019-08-25T21:04:48.841Z"
+  },
+  {
+    "id": 6,
+    "name": "Rustic",
+    "color_1": "#500123",
+    "color_2": "#000123",
+    "color_3": "#120123",
+    "color_4": "#800123",
+    "color_5": "#430123",
+    "project_id": 2,
+    "created_at": "2019-08-25T21:04:48.842Z",
+    "updated_at": "2019-08-25T21:04:48.842Z"
+  },
+  {
+    "id": 7,
+    "name": "Resonant",
+    "color_1": "#600123",
+    "color_2": "#060123",
+    "color_3": "#120123",
+    "color_4": "#003123",
+    "color_5": "#431123",
+    "project_id": 2,
+    "created_at": "2019-08-25T21:04:48.843Z",
+    "updated_at": "2019-08-25T21:04:48.843Z"
+  }
+]
+
+export const singleMockPalette = {
+  "id": 1,
+  "name": "Warm",
+  "color_1": "#000123",
+  "color_2": "#900123",
+  "color_3": "#J00123",
+  "color_4": "#000123",
+  "color_5": "#000123",
+  "project_id": 1,
+  "created_at": "2019-08-25T21:04:48.835Z",
+  "updated_at": "2019-08-25T21:04:48.835Z"
+}
