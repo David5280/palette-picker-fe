ca08d0f285d6bd75b24fa6ecae29d141c7b43b9c
David Engel <Davidjengel2012@gmail.com>
2019-08-26 18:30:40 -0600

    Added links and dynamic project

---
+++ src/Projects/Projects.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Route, Link } from 'react-browser-router';
+import SingleProject from '../SingleProject/SingleProject';
+
+export class Projects extends Component {
+  constructor(){
+    super()
+    this.state ={
+
+    }
+  }
+  allProjects = () => {
+    return this.props.projects.map(project => {
+      return (
+        <article className='single-project'>
+          <Link to={`/projects/${project.name.split(' ').join('')+'_'+project.id}`}>
+          <h2>{project.name}</h2>
+          </Link>
+        </article>
+      )
+    })
+  }
+  render() {
+    return (
+      <section >
+        {this.allProjects()}
+      </section>
+    )
+  }
+}
+
+export const mapStateToProps = state => ({
+  projects: state.projects
+});
+
+export default connect(mapStateToProps, null)(Projects);
