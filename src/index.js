cce24bdeeb6442f90d028e614e3ea529546d2b6b
David Engel <Davidjengel2012@gmail.com>
2019-08-26 18:31:00 -0600

    add hash router

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { rootReducer } from './reducers/rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { HashRouter } from 'react-browser-router';
+
 
 const store = createStore(rootReducer, composeWithDevTools())
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>
 , document.getElementById('root'));
 
