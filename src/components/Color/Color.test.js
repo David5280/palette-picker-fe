2c258e7d8c47a3fab695d0b253ff67386f46853d
David Engel <Davidjengel2012@gmail.com>
2019-08-28 17:13:17 -0600

    Add tests for Color.js

--- src/components/Color/Color.test.js
+++ src/components/Color/Color.test.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
-import { shallow } from 'enzyme'
+import Color from './Color';
+import { shallow } from 'enzyme';
 
 describe('Color Container', () => {
-  it('should do something', () => {
-
-  })
-})
+  let mockColorObj;
+  let wrapper;
+  beforeEach(() => {
+    mockColorObj = {
+      color: '#FFFEEE',
+      paletteID: 4,
+      dbKey: 'AYEE',
+    }
+    wrapper = shallow(<Color 
+      color={mockColorObj.color}
+      paletteID={mockColorObj.paletteID}
+      dbKey={mockColorObj.dbKey}
+      patchProject={jest.fn()}
+      hexcode={'#ddbbcc'}
+      index={1}
+      handleLock={jest.fn()}
+      icon={'/newIcon.png'}/>)
+  });
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  });
+  it('should call handleLock when the button is clicked', () => {
+    wrapper.find('.locked-icon').simulate('click');
+    expect(wrapper.instance().handleLock()).toHaveBeenCalled();
+  });
+});
