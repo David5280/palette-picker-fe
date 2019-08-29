ae0bf51bb30d3fd99744cc7c87f41d711dc2213c
Patrick Goulding <patrickgoulding@gmail.com>
2019-08-28 23:45:01 -0600

    WIP component did mount test on SingleProject

--- src/components/SingleProject/SingleProject.test.js
+++ src/components/SingleProject/SingleProject.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SingleProject } from './SingleProject';
 import { shallow } from 'enzyme';
+import { serverCall } from '../../fetchCalls/fetchCalls';
 
 jest.mock('../../fetchCalls/fetchCalls', () => ({
   serverCall: jest.fn().mockImplementation(() => {
@@ -11,12 +12,26 @@ jest.mock('../../fetchCalls/fetchCalls', () => ({
 describe('SingleProject', () => {
   let wrapper;
   let mockProjectID;
+  let instance;
   beforeEach(() => {
     mockProjectID = 3
     wrapper = shallow(<SingleProject id={mockProjectID}/>)
-    
+    instance = wrapper.instance()
   })
+
   it('should do match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('HAPPY: Should set the name on CDM', async () => {
+    // jest.spyOn(instance, 'serverCall')
+    instance.componentDidMount().then(
+      expect(wrapper.state('project_name')).toEqual()
+    )
+  })
+
+  it('HAPPY: Should call a patch fetch on patchPrject', () => {
+
+  })
+
 });
