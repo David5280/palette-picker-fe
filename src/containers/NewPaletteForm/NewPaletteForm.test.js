b1439cbbaea37fd943ea5b99d15e4719a6259dc3
Patrick Goulding <patrickgoulding@gmail.com>
2019-08-28 18:07:22 -0600

    WIP New Palette form testing

--- src/containers/NewPaletteForm/NewPaletteForm.test.js
+++ src/containers/NewPaletteForm/NewPaletteForm.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {NewPaletteForm} from './NewPaletteForm' 
+import { NewPaletteForm, mapStateToProps } from './NewPaletteForm' 
 import { shallow } from 'enzyme'
 import { mockColors, mockProjects } from '../../mockData';
 
@@ -17,6 +17,17 @@ let wrapper;
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should map state to props', () => {
+    const mockState = {
+      colors: mockColors,
+      projects: mockProjects
+    };
+
+    const mappedProps = mapStateToProps(mockState);
+    expect(mappedProps).toEqual(mockState);
+    
   })
 
 })
