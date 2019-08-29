064780f471e55b2e25338e8cdb464fd246bd348b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-29 20:38:49 +0000

    Deploy David5280/palette-picker-fe to github.com/David5280/palette-picker-fe.git:gh-pages

---
+++ precache-manifest.058df7f18b4afe834c971718209bf451.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc965981e09679c6a7e77e050995fcf",
+    "url": "/palette-picker-fe/index.html"
+  },
+  {
+    "revision": "f115cfecfb0cd07a8405",
+    "url": "/palette-picker-fe/static/css/main.544e9213.chunk.css"
+  },
+  {
+    "revision": "8655d627a6d2e610b10c",
+    "url": "/palette-picker-fe/static/js/2.60e2b2e4.chunk.js"
+  },
+  {
+    "revision": "f115cfecfb0cd07a8405",
+    "url": "/palette-picker-fe/static/js/main.ab0ad4bf.chunk.js"
+  },
+  {
+    "revision": "621c64cbe2af78fd2c00",
+    "url": "/palette-picker-fe/static/js/runtime~main.54601c64.js"
+  },
+  {
+    "revision": "2d92e7ee6558ad4aa6ecbf80ffdb46d9",
+    "url": "/palette-picker-fe/static/media/Rosalina.2d92e7ee.ttf"
+  },
+  {
+    "revision": "798e1aef08104b14d7e99c6472a4a5e9",
+    "url": "/palette-picker-fe/static/media/locked.798e1aef.svg"
+  },
+  {
+    "revision": "21855185dfdec9bd39dd328f66cfb64a",
+    "url": "/palette-picker-fe/static/media/unlocked.21855185.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/palette-picker-fe/precache-manifest.058df7f18b4afe834c971718209bf451.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/palette-picker-fe/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
